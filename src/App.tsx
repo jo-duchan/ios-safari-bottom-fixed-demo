7cd92f679ef55710e2e40335e5464abe51da6f54
jo-duchan <jo_duchan@icloud.com>
2023-11-01 17:45:50 +0900

    feat: 프로젝트 생성, 코어 기능 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import visualViewPortResize from "visualViewportResize";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [isIOS, setIsIOS] = useState<boolean>(false);
+  useEffect(() => {
+    setIsIOS(/iPhone|iPad|iPod/i.test(navigator.userAgent));
+
+    if (isIOS) {
+      visualViewPortResize();
+    }
+  }, [isIOS]);
+
+  if (!isIOS) {
+    return <div>아이폰 아님</div>;
+  }
+  return <div className="App">아이폰</div>;
 }
 
 export default App;
