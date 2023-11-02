d74ba2146f63f31c4113757c6d251a5eaa9cacec
jo-duchan <jo_duchan@icloud.com>
2023-11-02 16:59:23 +0900

    feat: Chips Component 작업

---
+++ src/components/Chips.tsx
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+
+const list = [
+  {
+    label: "None",
+  },
+  {
+    label: "VisualViewport",
+  },
+  {
+    label: "CSS",
+  },
+];
+
+interface Props {
+  onChange: (value: string) => void;
+}
+
+interface StyledProps {
+  active: boolean;
+}
+
+function Chips({ onChange }: Props) {
+  const [select, setSelect] = useState("NONE");
+
+  useEffect(() => {
+    onChange(select);
+  }, [select]);
+
+  const selectHandler = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setSelect(e.currentTarget.innerText.toLocaleUpperCase());
+  };
+
+  return (
+    <Container>
+      {list.map((chip) => (
+        <Chip
+          key={chip.label}
+          onClick={selectHandler}
+          active={chip.label.toLocaleUpperCase() === select}
+        >
+          {chip.label}
+        </Chip>
+      ))}
+    </Container>
+  );
+}
+
+export default Chips;
+
+const Container = styled.div`
+  display: flex;
+  gap: 8px;
+  padding-inline: 24px;
+  box-sizing: border-box;
+  margin-bottom: 52px;
+`;
+
+const Chip = styled.div<StyledProps>`
+  width: fit-content;
+  padding: 4px 10px;
+  box-sizing: border-box;
+  border-radius: 13px;
+  color: ${({ active }) => (active ? "#FFF" : "#696A6C")};
+  background: ${({ active }) => (active ? "#41A6EF" : "#F6F5F5")};
+  font-size: 14px;
+  font-weight: 600;
+  line-height: normal;
+  transition: color, background, 150ms ease-in-out;
+`;
